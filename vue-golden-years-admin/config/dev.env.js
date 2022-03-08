c7a46e52c8fce699886c26ab8b7b0de9114fd8d3
qingweico <17796706221@163.com>
2022-03-08 14:13:21 +0800

    2022-03-08

--- vue-golden-years-admin/config/dev.env.js
+++ vue-golden-years-admin/config/dev.env.js
@@ -6,13 +6,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
   //开发环境
-  ADMIN_API: '"http://localhost:8607/mogu-admin"',
-  PICTURE_API: '"http://localhost:8607/mogu-picture"',
-  WEB_API: '"http://localhost:8607/mogu-web"',
-  Search_API: '"http://localhost:8607/mogu-search"',
-  Spider_API: '"http://localhost:8607/mogu-spider"',
-  FILE_API: '"http://localhost:8600/"',
-  BLOG_WEB_URL: '"http://localhost:9527"',
-  SOLR_API: '"http://localhost:8080/solr"',
-  ELASTIC_SEARCH: '"http://localhost:5601"',
+  ADMIN_API: '"http://localhost:8004"',
 })
